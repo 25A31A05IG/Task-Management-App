2030f82e006b418606efbd9013f283363ace5b57
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-05-19 11:19:57 +0530

    Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [data, setData] = useState({
-    name: "",
+    username: "",
     email: "",
     password: "",
   });
@@ -20,7 +20,7 @@ function Register() {
 
   const register = async () => {
     try {
-      console.log("REGISTER DATA:", data);
+      console.log("DATA:", data);
 
       const res = await axios.post(
         "https://task-management-app-77tz.onrender.com/api/users/register",
@@ -40,81 +40,32 @@ function Register() {
   };
 
   return (
-    <div className="login-page">
+    <div>
+      <h1>Register</h1>
 
-      <div className="login-left">
-        <div className="login-box">
+      <input
+        name="username"
+        placeholder="username"
+        value={data.username}
+        onChange={handleChange}
+      />
 
-          <h1>Create Account</h1>
-          <p>Register to continue</p>
+      <input
+        name="email"
+        placeholder="email"
+        value={data.email}
+        onChange={handleChange}
+      />
 
-          {/* NAME */}
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter name"
-            value={data.name}
-            onChange={handleChange}
-          />
+      <input
+        name="password"
+        type="password"
+        placeholder="password"
+        value={data.password}
+        onChange={handleChange}
+      />
 
-          {/* EMAIL */}
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            value={data.email}
-            onChange={handleChange}
-            style={{ marginTop: "15px" }}
-          />
-
-          {/* PASSWORD */}
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter password"
-            value={data.password}
-            onChange={handleChange}
-            style={{ marginTop: "15px" }}
-          />
-
-          {/* BUTTON */}
-          <button
-            onClick={register}
-            style={{
-              width: "100%",
-              marginTop: "20px",
-            }}
-          >
-            Register
-          </button>
-
-          <p style={{ marginTop: "20px" }}>
-            Already have an account?{" "}
-            <span
-              onClick={() => navigate("/")}
-              style={{
-                color: "#2563eb",
-                cursor: "pointer",
-                fontWeight: "bold",
-              }}
-            >
-              Login
-            </span>
-          </p>
-
-        </div>
-      </div>
-
-      <div className="login-right">
-        <div className="floating-card">
-          <h2>Stay Organized 📋</h2>
-          <p style={{ marginTop: "10px" }}>
-            Create tasks, track progress,
-            manage priorities and improve productivity.
-          </p>
-        </div>
-      </div>
-
+      <button onClick={register}>Register</button>
     </div>
   );
 }
