1293f4fe989673841089c7e9a0067daa11aeb364
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-05-19 13:47:41 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "./Layout";
+import Layout from "./components/Layout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
