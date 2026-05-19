e784a1c16e74da384585a6e91b0b25f7f25fc0be
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-05-19 10:41:22 +0530

    Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 function Register() {
 
   const [data, setData] = useState({
-    username: "",
+    name: "",
     email: "",
     password: "",
   });
