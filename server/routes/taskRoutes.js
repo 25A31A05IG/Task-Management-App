9eefdf0cf5b199c4e9df8554eee14873346d1a74
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-05-19 11:29:41 +0530

    taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const Task = require("../models/Task");
+const auth = require("../middleware/auth");
 
-const {
-  createTask,
-  getTasks,
-  deleteTask,
-  toggleTask,
-  updateTask,
-} = require("../controllers/taskController");
+// GET ONLY USER TASKS
+router.get("/", auth, async (req, res) => {
+  const tasks = await Task.find({ user: req.user.id });
+  res.json(tasks);
+});
 
-router.post("/", createTask);
-router.get("/:userId", getTasks);
-router.delete("/:id", deleteTask);
-router.put("/:id", toggleTask);
-router.put("/edit/:id", updateTask);
+// CREATE TASK
+router.post("/", auth, async (req, res) => {
+  const { title, description } = req.body;
+
+  const task = await Task.create({
+    title,
+    description,
+    user: req.user.id,
+  });
+
+  res.json(task);
+});
 
 module.exports = router;
