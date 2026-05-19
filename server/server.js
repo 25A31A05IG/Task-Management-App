a14274363f3bda74df8ec62af65ed290455d3e85
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-05-19 12:02:06 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -5,25 +5,17 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-app.use(cors({
-  origin: "*"
-}));
-
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-// connect DB
 connectDB();
 
-// test route
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-// ✅ IMPORTANT ROUTE MOUNTING
 app.use("/api/users", require("./routes/userRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log("Server running"));
