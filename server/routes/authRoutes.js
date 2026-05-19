6b1b620d16a12cd268950cf90b5193f0c8f9c0ff
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-05-19 11:28:26 +0530

    authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
-const { register, login } = require("../controllers/authController");
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-router.post("/register", register);
-router.post("/login", login);
+    const existing = await User.findOne({ email });
+    if (existing) return res.status(400).json({ message: "User exists" });
+
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    res.json({ message: "Registered", user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user || user.password !== password) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      "secretkey",
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      token,
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
