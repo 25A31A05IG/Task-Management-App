cac48683b8c1c5a8dee9f39147c0cb6f62e4ef29
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-05-19 13:08:33 +0530

    authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,113 +1,90 @@
 const User = require("../models/User");
-
 const bcrypt = require("bcryptjs");
-
 const jwt = require("jsonwebtoken");
-
 
 // REGISTER
 exports.register = async (req, res) => {
-
   try {
+    const { username, email, password } = req.body;
 
-    const {
-      username,
-      email,
-      password,
-    } = req.body;
-
-    // CHECK EXISTING USER
-    const existingUser =
-      await User.findOne({ email });
+    // check existing user
+    const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-
       return res.status(400).json({
         message: "User already exists",
       });
     }
 
-    // HASH PASSWORD
-    const hashedPassword =
-      await bcrypt.hash(password, 10);
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    // CREATE USER
+    // create user
     const user = await User.create({
       username,
       email,
       password: hashedPassword,
     });
 
+    // IMPORTANT: return clean user (no password)
     res.status(201).json({
-      message:
-        "Registration successful",
-      user,
+      message: "Registration successful",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
     });
-
   } catch (err) {
-
     console.log(err);
-
     res.status(500).json({
       message: err.message,
     });
   }
 };
 
-
 // LOGIN
 exports.login = async (req, res) => {
-
   try {
+    const { email, password } = req.body;
 
-    const {
-      email,
-      password,
-    } = req.body;
-
-    // FIND USER
-    const user =
-      await User.findOne({ email });
+    // find user
+    const user = await User.findOne({ email });
 
     if (!user) {
-
       return res.status(400).json({
-        message:
-          "Invalid email or password",
+        message: "Invalid email or password",
       });
     }
 
-    // CHECK PASSWORD
-    const isMatch =
-      await bcrypt.compare(
-        password,
-        user.password
-      );
+    // check password
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-
       return res.status(400).json({
-        message:
-          "Invalid email or password",
+        message: "Invalid email or password",
       });
     }
 
-    // TOKEN
+    // generate token
     const token = jwt.sign(
       { id: user._id },
       "secretkey",
       { expiresIn: "1d" }
     );
 
+    // IMPORTANT: return clean user (no password)
     res.json({
       token,
-      user,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
     });
 
   } catch (err) {
-
     console.log(err);
-
     res.status(500).json({
       message: err.message,
     });
